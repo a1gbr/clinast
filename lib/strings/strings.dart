3a7f5780e5576178b417ca155cdfa965862538fb
João Gabriel Alves <joao.alves.1129911@sga.pucminas.br>
2023-08-24 13:12:59 -0300

    titulo de pagina inicial, strings nao dinamicas #5

---
+++ lib/strings/strings.dart
@@ -0,0 +1,5 @@
+const tituloPagina = "Titulo da Pagina";
+const descricaoPagina = "Descricao da Pagina";
+
+const textoBotaoExportar = "Exportar";
+const textoBotaoAdicionar = "Novo Item";
---
+++ lib/widgets/titulo_pagina.dart
@@ -0,0 +1,77 @@
+import 'package:clinast/strings/strings.dart';
+import 'package:flutter/material.dart';
+
+class TituloPagina extends StatelessWidget {
+  const TituloPagina({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+    const TextStyle botaoStyle = TextStyle(fontSize: 18);
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  tituloPagina,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                ),
+                Text(
+                  descricaoPagina,
+                  style: TextStyle(fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Center(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              children: <Widget>[
+                ElevatedButton(
+                  style: style,
+                  onPressed: () {},
+                  child: const Row(
+                    children: [
+                      Icon(
+                        (Icons.upgrade),
+                      ),
+                      Padding(padding: EdgeInsets.all(2)),
+                      Text(
+                        textoBotaoExportar,
+                        style: botaoStyle,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  style: style,
+                  onPressed: () {},
+                  child: const Row(
+                    children: [
+                      Icon(
+                        (Icons.add),
+                      ),
+                      Padding(padding: EdgeInsets.all(2)),
+                      Text(
+                        textoBotaoAdicionar,
+                        style: botaoStyle,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
