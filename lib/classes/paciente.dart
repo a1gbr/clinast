562ae2a561ddfd714096893aa3efc41ff7d964f6
João Gabriel Alves <joao.alves.1129911@sga.pucminas.br>
2023-09-01 04:11:22 -0300

    base pessoa, classe func, med e paciente adicionados

---
+++ lib/classes/paciente.dart
@@ -0,0 +1,37 @@
+import 'package:clinast/classes/pessoa.dart';
+
+enum _GeradorPaciente { instance }
+
+class Paciente extends Pessoa {
+  final String altura;
+  final String peso;
+  final List<String> medicamentos;
+
+  Paciente(
+      {required String nome,
+      required String dataNascimento,
+      required String rua,
+      required String bairro,
+      required String cidade,
+      required String cep,
+      required String telefone,
+      required String cpf,
+      required this.altura,
+      required this.peso,
+      required this.medicamentos})
+      : super(
+          nome: nome,
+          dataNascimento: dataNascimento,
+          rua: rua,
+          bairro: bairro,
+          cidade: cidade,
+          cep: cep,
+          telefone: telefone,
+          cpf: cpf,
+        );
+
+  @override
+  String toString() {
+    return 'Paciente(id: $id, nome: $nome, dataNascimento: $dataNascimento, rua: $rua, bairro: $bairro, cidade: $cidade, cep: $cep, telefone: $telefone, cpf: $cpf, altura: $altura, função: $peso, medicamentos: $medicamentos)';
+  }
+}
